468f4a98a965fc8a0f30b4fa4e63f191433679c9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-14 23:31:00 -0700

    Added tests for new dynamic plots. Removed newlines.

---
+++ tests/testthat/test-DifferentialStatisticsTable.R
@@ -0,0 +1,63 @@
+# library(testthat); library(iSEEu); source("test-DifferentialStatisticsTable.R")
+
+se <- SummarizedExperiment(list(logcounts=matrix(0, 10, 4)),
+    rowData=DataFrame(PValue=runif(10), LogFC=rnorm(10), AveExpr=rnorm(10)))
+dimnames(se) <- list(1:nrow(se), letters[seq_len(ncol(se))])
+
+test_that("DifferentialStatisticsTable constructor works correctly", {
+    out <- DifferentialStatisticsTable()
+    expect_error(DifferentialStatisticsTable(LogFC=-1), "non-negative")
+    expect_error(DifferentialStatisticsTable(TestMethod="a"), "must be")
+})
+
+test_that("DifferentialStatisticsTable interface definition works correctly", {
+    x <- DifferentialStatisticsTable()
+    expect_error(.defineDataInterface(x, se, list()), NA)
+
+    expect_false(.hideInterface(x, "ColumnSelectionSource"))
+    expect_true(.hideInterface(x, "RowSelectionSource"))
+
+    expect_identical(.fullName(x), "Differential statistics table")
+    expect_is(.panelColor(x), "character")
+})
+
+test_that("DifferentialStatisticsTable caching and refinement works correctly", {
+    x <- DifferentialStatisticsTable()
+    expect_identical(x[["Assay"]], NA_character_)
+
+    expect_warning(x2 <- .refineParameters(x, se), "no valid")
+    expect_identical(x2, NULL)
+
+    se <- .cacheCommonInfo(x, se)
+    expect_identical(.getCachedCommonInfo(se, "DifferentialStatisticsTable")$valid.assay.names, "logcounts")
+
+    x <- .refineParameters(x, se)
+    expect_identical(x[["Assay"]], "logcounts")
+})
+
+test_that("DifferentialStatisticsTable generates the table correctly", {
+    env <- new.env()
+    env$col_selected <- list()
+    env$se <- se
+
+    x <- DifferentialStatisticsTable()
+    se <- .cacheCommonInfo(x, se)
+    x <- .refineParameters(x, se)
+
+    out <- .generateTable(x, env)
+    expect_identical(nrow(env$tab), 0L)        
+
+    env$col_selected <- list(saved1=letters[1:2])
+    out <- .generateTable(x, env)
+    expect_identical(nrow(env$tab), 0L)        
+
+    env$col_selected <- list(active=letters[1:2])
+    out <- .generateTable(x, env)
+    expect_identical(nrow(env$tab), nrow(se))
+    past <- ncol(env$tab)
+
+    env$col_selected <- list(active=letters[1:2], saved1=letters[3:4], saved2=letters[2:3])
+    out <- .generateTable(x, env)
+    expect_identical(nrow(env$tab), nrow(se))
+    expect_identical(ncol(env$tab), past+1L)
+})
