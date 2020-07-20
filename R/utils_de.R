77a2a61e05a92bba38b6a16831c7d33353d660b4
Aaron Lun <LTLA@users.noreply.github.com>
2020-07-20 15:54:40 -0700

    Perform pattern matching for DE-related globals to identify appropriate fields.
    Also use pattern matching for MAPlots and VolcanoPlots, closes #65.

--- R/utils_de.R
+++ R/utils_de.R
@@ -33,45 +33,45 @@ NULL
 #' @export
 #' @rdname utils-de
 .getAcceptablePValueFields <- function() {
-    .Deprecated(new="getPValueFields")
-    getPValueFields()
+    .Deprecated(new="getPValuePattern")
+    getPValuePattern()
 }
 
 #' @export
 #' @rdname utils-de
 .getAcceptableLogFCFields <- function() {
-    .Deprecated(new="getLogFCFields")
-    getLogFCFields()
+    .Deprecated(new="getLogFCPattern")
+    getLogFCPattern()
 }
 
 #' @export
 #' @rdname utils-de
 .getAcceptableAveAbFields <- function() {
-    .Deprecated(new="getAveAbFields")
-    getAveAbFields()
+    .Deprecated(new="getAveAbPattern")
+    getAveAbPattern()
 }
 
 #' @export
 #' @rdname utils-de
 .setAcceptablePValueFields <- function(value) {
-    .Deprecated(new="setPValueFields")
-    setPValueFields(value)
+    .Deprecated(new="setPValuePattern")
+    setPValuePattern(value)
     invisible(NULL)
 }
 
 #' @export
 #' @rdname utils-de
 .setAcceptableLogFCFields <- function(value) {
-    .Deprecated(new="setLogFCFields")
-    setLogFCFields(value)
+    .Deprecated(new="setLogFCPattern")
+    setLogFCPattern(value)
     invisible(NULL)
 }
 
 #' @export
 #' @rdname utils-de
 .setAcceptableAveAbFields <- function(value) {
-    .Deprecated(new="setAveAbFields")
-    setAveAbFields(value)
+    .Deprecated(new="setAveAbPattern")
+    setAveAbPattern(value)
     invisible(NULL)
 }
 
@@ -129,4 +129,17 @@ NULL
         x[[field]] <- x[[choices]][1]
     }
     x
+}
+
+.match_acceptable_fields <- function(provided, globals, available) {
+    if (.needs_filling(provided)) {
+        provided <- globals
+        okay <- logical(length(available))
+        for (x in provided) {
+            okay <- okay | grepl(x, available, fixed=TRUE)
+        } 
+        available[okay]
+    } else {
+        intersect(available, provided)
+    }
 }
